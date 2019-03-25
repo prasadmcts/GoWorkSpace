c3752dc9c815a9cdaebc611f67f35c5816a842c4
Prasad Kanaparthi <prasadmcts@gmail.com>
2019-03-25 17:55:38 +0530

    examples

--- server.go
+++ server.go
@@ -5,19 +5,10 @@ import (
 	"net/http"
 )
 
-func ping(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("pong"))
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-}
-
 func main() {
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/ping", ping)
-	fmt.Println("http://localhost:8080/SomeName")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		panic(err)
-	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	})
+
+	http.ListenAndServe(":80", nil)
 }
